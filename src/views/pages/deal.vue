71ae9d5cd97f08c8904bdd6876d13f2b14da4c4c
MingYu-tju <1597232338@qq.com>
2024-02-29 15:47:35 +0800

    业务处理新增关键节点弹窗，传参格式修改

--- src/views/pages/deal.vue
+++ src/views/pages/deal.vue
@@ -6,8 +6,8 @@
         v-model="stageValue"
         class="m-2"
         placeholder="Select"
-        style="margin-left: 30px" 
-        @change="filterMember()"
+        style="margin-left: 30px"
+        @change="filterMember(true)"
       >
         <el-option
           v-for="item in options"
@@ -22,9 +22,10 @@
       <el-select
         v-model="subStageValue"
         class="m-2"
-        placeholder="Select"   
+        placeholder="Select"
         style="margin-left: 30px"
-        @change="filterMember()">
+        @change="filterMember(false)"
+      >
         <el-option
           v-for="item in options_little[stageValue]"
           :key="item.value"
@@ -34,7 +35,12 @@
       </el-select>
       <!-- <el-button style="margin-left: 30px" color="#c7242f" @click="filterMember()">筛选</el-button>
       疑似反人类按钮 我先给扣了 用上面的@change="filterMember" -->
-      <el-button style="margin-left: 30px" color="#c7242f" @click="processAccess()">通过选中成员</el-button>
+      <el-button
+        style="margin-left: 30px"
+        color="#c7242f"
+        @click="multiProcessAccess()"
+        >通过选中成员</el-button
+      >
     </div>
   </div>
   <div class="Main">
@@ -51,18 +57,18 @@
       </el-table-column>
       <el-table-column property="sno" label="学号" />
       <el-table-column property="identity" label="身份">
-        {{  options[stageValue].label  }}
+        {{ options[stageValue].label }}
       </el-table-column>
       <el-table-column label="操作">
-          <template #default="scope">
-            <el-button
-              style="color: #21b339"
-              link
-              @click="handleCheck(scope.row)"
-              >通过</el-button
-            >
-          </template>
-        </el-table-column>
+        <template #default="scope">
+          <el-button
+            style="color: #21b339"
+            link
+            @click="processAcsess(scope.row)"
+            >通过</el-button
+          >
+        </template>
+      </el-table-column>
     </el-table>
     <el-config-provider :locale="zhCn">
       <el-pagination
@@ -78,8 +84,67 @@
       />
     </el-config-provider>
   </div>
+  <!----------------------------------------弹窗------------------------------------------->>
+  <div class="modal" v-if="visible">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <div class="modal-right">
+        <div class="font-1">请填写以下成员本次状态变更时间：</div>
+        <div style="margin-top: 20px; display: flex" v-if="showMuti">
+          <div class="font-4">批量填写：</div>
+          <el-date-picker
+            v-model="date"
+            type="datetime"
+            size="small"
+            placeholder="Select date and time"
+            format="YYYY-MM-DD-HH:mm"
+            value-format="YYYY-MM-DDTHH:mm:ss.sss"
+          />
+          <el-button
+            color="#c7242f"
+            style="width: 50px; height: 24px"
+            @click="batchFilling"
+            >填写</el-button
+          >
+        </div>
+        <el-table
+          style="width: 90%; margin-top: 20px; margin-left: 20px"
+          max-height="500px"
+          size="small"
+          :cell-style="{ padding: '2px', fontSize: '13px' }"
+          :data="selectData"
+          :header-cell-style="{ background: '#FFF8F9', color: '#2F2F2F' }"
+        >
+          <el-table-column prop="userName" label="姓名" min-width="30%">
+          </el-table-column>
+          <el-table-column prop="sno" label="学号" min-width="45%">
+          </el-table-column>
+          <el-table-column label="状态变更时间" prop="passAt">
+            <template #default="scope">
+              <el-date-picker
+                type="datetime"
+                size="small"
+                placeholder="Select date and time"
+                v-model="scope.row.passAt"
+                format="YYYY-MM-DD-HH:mm"
+                value-format="YYYY-MM-DDTHH:mm:ss.sss"
+              ></el-date-picker>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div class="modal-tail">
+        <el-button color="#c7242f" @click="confirmUpdateBatch(true)"
+          >确认更改</el-button
+        >
+        <div style="color: #c7242f; margin-top: 5px">
+          *本操作无法回退，请确认无误后再操作
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-  
+
 <script lang="ts" setup>
 import { ref, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
@@ -88,11 +153,18 @@ import zhCn from "element-plus/es/locale/lang/zh-cn";
 import { reactive } from "vue";
 import { processFilter, updateBatch } from "../../api/stateControl";
 
-
 interface User {
   userName: string;
   sno: string;
   id: number;
+  passAt: String;
+}
+
+interface User_R {
+  userId: number;
+  progressId: number;
+  status: number;
+  passAt: String;
 }
 
 const $route = useRoute();
@@ -100,6 +172,9 @@ const $router = useRouter();
 const stageValue = ref(0);
 const subStageValue = ref(0);
 const input = ref("");
+const visible = ref(false);
+const showMuti = ref(true);
+const date = ref("");
 const options = [
   {
     label: "入党申请人前",
@@ -120,7 +195,7 @@ const options = [
   {
     label: "中共预备党员",
     value: 4,
-  }
+  },
 ];
 const options_little = [
   [
@@ -160,7 +235,7 @@ const options_little = [
       label: "入党积极分子",
       value: 4,
       stage: 6,
-    }
+    },
   ],
   [
     {
@@ -207,7 +282,7 @@ const options_little = [
       label: "发展对象",
       value: 8,
       stage: 15,
-    }
+    },
   ],
   [
     {
@@ -221,186 +296,255 @@ const options_little = [
       stage: 17,
     },
     {
-      label: "通过发展对象党校培训",
+      label: "政审",
       value: 2,
       stage: 18,
     },
     {
-      label: "材料齐全",
+      label: "通过发展对象党校培训",
       value: 3,
       stage: 19,
     },
     {
-      label: "支部综合审查",
+      label: "材料齐全",
       value: 4,
       stage: 20,
     },
     {
-      label: "上级党委预审并公示",
+      label: "支部综合审查",
       value: 5,
       stage: 21,
     },
     {
-      label: "填写入党申请书",
+      label: "上级党委预审并公示",
       value: 6,
       stage: 22,
     },
     {
-      label: "党员发展大会",
+      label: "填写入党申请书",
       value: 7,
       stage: 23,
     },
     {
-      label: "党委谈话、审批",
+      label: "党员发展大会",
       value: 8,
       stage: 24,
     },
     {
-      label: "报再上一级党委组织部门备案",
+      label: "党委谈话、审批",
       value: 9,
       stage: 25,
     },
     {
-      label: "中共预备党员",
+      label: "报再上一级党委组织部门备案",
       value: 10,
       stage: 26,
-    }
+    },
+    {
+      label: "中共预备党员",
+      value: 11,
+      stage: 27,
+    },
   ],
   [
     {
       label: "入党宣誓",
       value: 0,
-      stage: 27,
+      stage: 28,
     },
     {
       label: "通过预备党员培训",
       value: 1,
-      stage: 28,
+      stage: 29,
     },
     {
       label: "季度递交个人小结",
       value: 2,
-      stage: 29,
+      stage: 30,
     },
     {
       label: "参加组织生活",
       value: 3,
-      stage: 30,
+      stage: 31,
     },
     {
       label: "支部按季度进行考察",
       value: 4,
-      stage: 31,
+      stage: 32,
     },
     {
       label: "提出转正申请",
       value: 5,
-      stage: 32,
+      stage: 33,
     },
     {
       label: "征求党员、群众意见",
       value: 6,
-      stage: 33,
+      stage: 34,
     },
     {
       label: "听取党小组、介绍人意见",
       value: 7,
-      stage: 34,
+      stage: 35,
     },
     {
       label: "转正公示",
       value: 8,
-      stage: 35,
+      stage: 36,
     },
     {
       label: "转正大会",
       value: 9,
-      stage: 36,
+      stage: 37,
     },
     {
       label: "党委审批",
       value: 10,
-      stage: 37,
+      stage: 38,
     },
     {
       label: "党员书记谈话",
       value: 11,
-      stage: 38,
+      stage: 39,
     },
     {
       label: "中共党员",
       value: 12,
-      stage: 39,
-    }
+      stage: 40,
+    },
   ],
-
-
+];
+const keyNode = [
+  {
+    label: "递交入党申请书",
+    value: 0,
+    stage: 0,
+  },
+  {
+    label: "群团组织推优",
+    value: 1,
+    stage: 3,
+  },
+  {
+    label: "入党积极分子",
+    value: 4,
+    stage: 6,
+  },
+  {
+    label: "发展对象",
+    value: 8,
+    stage: 15,
+  },
+  {
+    label: "党员发展大会",
+    value: 8,
+    stage: 24,
+  },
 ];
 const tableData = ref(<User[]>[]);
-const currentPage = ref(1);
+const selectData = ref<User[]>([]);
 const multipleTableRef = ref<InstanceType<typeof ElTable>>();
 const multipleSelection = ref<User[]>([]);
 const pageSize = ref(<number>15); //每页显示数量
-let memberRawData = ref(<User[]>[])
-let memberNum = ref(0)  //总长度
-let PageNum = ref(1)  //当前页码
+let memberRawData = ref(<User[]>[]);
+let memberNum = ref(0); //总长度
+let PageNum = ref(1); //当前页码
 
-const filterMember = async () => {
-  let RawData:{code:number,data:[]} = await processFilter(options_little[stageValue.value][subStageValue.value].stage)
-  if(RawData.code == 0){
-    memberRawData.value = RawData.data
-    memberNum.value = RawData.data.length
-    tableData.value = memberRawData.value.slice((PageNum.value - 1) * pageSize.value, PageNum.value * pageSize.value);
+const filterMember = async (init: boolean) => {
+  if (init) subStageValue.value = 0; //不加这个会有bug
+  let RawData: { code: number; data: [] } = await processFilter(
+    options_little[stageValue.value][subStageValue.value].stage
+  );
+  if (RawData.code == 0) {
+    memberRawData.value = RawData.data;
+    memberNum.value = RawData.data.length;
+    tableData.value = memberRawData.value.slice(
+      (PageNum.value - 1) * pageSize.value,
+      PageNum.value * pageSize.value
+    );
   }
-}
+};
 
-const processAccess = async () => {
-  let ids:number[] = []
-  let data:any = {ids:<number[]>[],processIds:<number[]>[],status:[1]}
-  multipleSelection.value.forEach((member) => {
-    ids.push(member.id)
-  })
-  data.ids = ids
-  data.processIds = [options_little[stageValue.value][subStageValue.value].stage]
-  let res:{code:number} = await updateBatch(data)
-  if(res.code == 0){
-    ElMessage.success('修改成功')
-    filterMember()
+//多选
+const multiProcessAccess = async () => {
+  selectData.value = multipleSelection.value;
+  showMuti.value = true;
+  for (let item of keyNode) {
+    if (
+      options_little[stageValue.value][subStageValue.value].stage == item.stage
+    ) {
+      showModal(); //关键节点
+      return;
+    }
   }
-}
+  //普通节点
+  selectData.value.forEach((member) => {
+    member.passAt = "";
+  });
+  confirmUpdateBatch(false);
+};
 
+//个人
+const processAcsess = async (row: User) => {
+  selectData.value = [row];
+  showMuti.value = false;
+  for (let item of keyNode) {
+    if (
+      options_little[stageValue.value][subStageValue.value].stage == item.stage
+    ) {
+      showModal();
+      return;
+    }
+  }
+
+  selectData.value.forEach((member) => {
+    member.passAt = "";
+  });
+  confirmUpdateBatch(false);
+};
+
+const confirmUpdateBatch = async (isKeyNode: boolean) => {
+  if (isKeyNode) {
+    //判断时间不能为空
+    for (let item of selectData.value)
+      if (!item.passAt) {
+        ElMessage.error("不能为空");
+        return;
+      }
+  }
+  let data: User_R[] = [];
+  selectData.value.forEach((member) => {
+    data.push({
+      userId: member.id,
+      progressId: options_little[stageValue.value][subStageValue.value].stage,
+      status: 1,
+      passAt: member.passAt,
+    });
+  });
+  let res: { code: number } = await updateBatch(data);
+  if (res.code == 0) {
+    ElMessage.success("修改成功");
+    filterMember(false);
+    closeModal();
+  }
+};
 const handleSizeChange = () => {
-  tableData.value = memberRawData.value.slice((PageNum.value - 1) * pageSize.value, PageNum.value * pageSize.value);
+  tableData.value = memberRawData.value.slice(
+    (PageNum.value - 1) * pageSize.value,
+    PageNum.value * pageSize.value
+  );
 };
 
 const handleCurrentChange = () => {
-  tableData.value = memberRawData.value.slice((PageNum.value - 1) * pageSize.value, PageNum.value * pageSize.value);
+  tableData.value = memberRawData.value.slice(
+    (PageNum.value - 1) * pageSize.value,
+    PageNum.value * pageSize.value
+  );
 };
-
-const handleCheck = async (row: User) => {
-  let data:any = {ids:[row.id],processIds:<number[]>[],status:[1]}
-  data.processIds = [options_little[stageValue.value][subStageValue.value].stage]
-  let res:{code:number} = await updateBatch(data)
-  if(res.code == 0){
-    ElMessage.success('修改成功')
-    filterMember()
-  }
-};
-
 
 onMounted(async () => {
-  filterMember()
-})
-
-const formInline = reactive({
-  user: "",
-  region: "",
-  date: "",
+  filterMember(true);
 });
-
-const onSubmit = () => {
-  console.log("submit!");
-};
 
 const toggleSelection = (rows?: User[]) => {
   if (rows) {
@@ -419,8 +563,38 @@ const handleSelectionChange = (val: User[]) => {
   multipleSelection.value = val;
 };
 
+const batchFilling = () => {
+  multipleSelection.value.forEach((member) => {
+    member.passAt = date.value;
+  });
+};
+const showModal = () => {
+  visible.value = true;
+  stop();
+};
+const closeModal = () => {
+  visible.value = false;
+  move();
+};
+
+// 记录页面滚动位置
+const pageLocation = ref();
+
+// 遮罩层外禁止滚动
+function stop() {
+  let scrollTop = window.scrollY;
+  pageLocation.value = scrollTop;
+  document.body.style.position = "fixed";
+  document.body.style.top = "-" + scrollTop + "px";
+}
+
+// 取消滑动限制
+function move() {
+  document.body.style.position = "static";
+  window.scrollTo(0, pageLocation.value);
+}
 </script>
-  
+
 <style scoped>
 .Main {
   height: 779px;
@@ -439,5 +613,72 @@ const handleSelectionChange = (val: User[]) => {
 .el-pagination.is-background .el-pager li {
   border-radius: 50%;
 }
+
+.modal {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: 1000;
+  left: 0;
+  top: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+.modal-content {
+  position: relative;
+  background-color: #fff;
+  border: 1px solid #888;
+  margin: 5% auto;
+  width: 600px;
+  height: 700px;
+  z-index: 1001;
+  display: flex;
+}
+
+.modal-right {
+  width: 90%;
+  margin-left: 10%;
+}
+.modal-tail {
+  width: 100%;
+  height: 70px;
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.font-1 {
+  margin-top: 20px;
+  margin-left: 20px;
+  font-size: 20px;
+  font-weight: 400;
+}
+.font-2 {
+  margin-left: 20px;
+  font-size: 16px;
+  font-weight: 400;
+}
+.font-3 {
+  margin-top: 3px;
+  margin-left: 20px;
+  color: #c7242f;
+}
+.font-4 {
+  margin-left: 20px;
+}
+
+.close {
+  position: absolute;
+  right: 20px;
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
 </style>
-  
